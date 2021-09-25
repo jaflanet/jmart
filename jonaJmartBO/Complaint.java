3d8a7a1908b9e80976e3db860f9d477182c0bfd5
RLoo <jonathanfaren@gmail.com>
2021-09-25 11:03:51 +0700

    modul3_rev

--- jonaJmartBO/Complaint.java
+++ jonaJmartBO/Complaint.java
@@ -1,10 +1,26 @@
 package jonaJmartBO;
 
+public abstract class Complaint extends Transaction implements FileParser
+{
+    public int paymentId;
+    public String desc;
 
-/**
- * Write a description of class Complaint here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-
+    public Complaint(int id, Payment payment, String desc){
+        super(id, 0, 0);
+    }
+    public Complaint(int id, int buyerId, int storeId, int paymentId, String desc){
+        super(id, buyerId, storeId);
+        this.paymentId = paymentId;
+        this.desc = desc;
+    }
+    @Override
+    public boolean read(String content){
+        return false;
+    }
+    public boolean validate(){
+        return false;
+    }
+    public Transaction perform(){
+        return null;
+    }
+}
--- jonaJmartBO/Payment.java
+++ jonaJmartBO/Payment.java
@@ -7,4 +7,27 @@ package jonaJmartBO;
  * @author (your name)
  * @version (a version number or a date)
  */
-
+public abstract class Payment extends Transaction implements FileParser
+{
+    public int productId;
+    public ShipmentDuration shipmentDuration;
+    public Payment(int id, int buyerId, Product product, ShipmentDuration shipmentDuration){
+        super(id, buyerId, 0);
+        this.shipmentDuration = shipmentDuration;
+    }
+    public Payment(int id, int buyerId, int storeId, int productId, ShipmentDuration shipmentDuration){
+        super(id, buyerId, storeId);
+        this.productId = productId;
+        this.shipmentDuration = shipmentDuration;
+    }
+    @Override
+    public boolean read(String content){
+        return false;
+    }
+    public boolean validate(){
+        return false;
+    }
+    public Transaction perform(){
+        return null;
+    }
+}
