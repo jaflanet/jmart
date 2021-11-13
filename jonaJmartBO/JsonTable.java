e3da0febf1bddaa249651a17a9144f3ebc8e4102
RLoo <jonathanfaren@gmail.com>
2021-11-13 11:03:18 +0700

    cs modul 7 rev

--- jonaJmartBO/JsonTable.java
+++ jonaJmartBO/JsonTable.java
@@ -13,25 +13,19 @@ public class JsonTable<T> extends Vector{
 	private static final Gson gson = new Gson();
 	
 	 public JsonTable(Class<T> clazz, String filepath) throws IOException{
-	        this.filepath = filepath;
-//	        try{
-//	            Class<T[]> array = (Class<T[]>) Array.newInstance(clazz, 0).getClass();
-//	            T[] hasil = JsonTable.readJson(array, filepath);
-//	            Collections.addAll(this, hasil);
-//	        }
-//	        catch(FileNotFoundException e){
-//	            File file = new File(filepath);
-//	        }
+		 this.filepath = filepath;
+	        try{
+	            Class<T[]> array = (Class<T[]>) Array.newInstance(clazz, 0).getClass();
+	            T[] hasil = JsonTable.readJson(array, filepath);
+	            Collections.addAll(this, hasil);
+	        }
+	        catch(FileNotFoundException e){
+	            File file = new File(filepath);
+	        }
 	 }
 	public static <T> T readJson (Class<T> clazz, String filepath) throws FileNotFoundException{
-		 T Jsonreader = null;
-		  try {
-		   final JsonReader read = new JsonReader(new FileReader(filepath));
-		   Jsonreader = gson.fromJson(read, clazz);
-		  } catch (FileNotFoundException ex) {
-		   ex.printStackTrace();
-		  }
-		  return Jsonreader;
+		final JsonReader read = new JsonReader(new FileReader(filepath));
+        return gson.fromJson(read, clazz);
 	}
 	
 	public void writeJson () throws IOException{
@@ -39,7 +33,7 @@ public class JsonTable<T> extends Vector{
 	}
 	
 	 public static void writeJson(Object object, String filepath) throws IOException {
-		  final FileWriter fileWriter = new FileWriter(filepath,true);
+		  final FileWriter fileWriter = new FileWriter(filepath);
 		  fileWriter.write(gson.toJson(object));
 		  fileWriter.close();
 		 }
