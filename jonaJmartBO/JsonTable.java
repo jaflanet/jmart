030f638a2084d9b70c90bf203d992095b03453d5
RLoo <jonathanfaren@gmail.com>
2021-11-15 21:15:32 +0700

    Post test modul 7

--- jonaJmartBO/JsonTable.java
+++ jonaJmartBO/JsonTable.java
@@ -13,23 +13,25 @@ public class JsonTable<T> extends Vector<T>{
 
     public JsonTable(Class<T> clazz, String filepath) throws IOException {
         this.filepath = filepath;
-        File f = new File(filepath);
-        f.getParentFile().mkdirs();
-        f.createNewFile();
+        File fileReader = new File(filepath);
+        fileReader.getParentFile().mkdirs();
+        fileReader.createNewFile();
         Class<T[]> arrayType = (Class<T[]>) Array.newInstance(clazz, 0).getClass();
         T[] loaded = readJson(arrayType, filepath);
         if (loaded != null) {
             Collections.addAll(this, loaded);
         }
     }
+    
     public static <T> T readJson(Class<T> clazz, String filepath) throws FileNotFoundException{
-        JsonReader freader = new JsonReader(new FileReader(filepath));
-        return gson.fromJson(freader, clazz);
+        JsonReader filereader = new JsonReader(new FileReader(filepath));
+        return gson.fromJson(filereader, clazz);
     }
 
     public void writeJson () throws IOException {
         writeJson(this, filepath);
     }
+    
     public static void writeJson(Object object, String filepath) throws IOException{
         FileWriter writer = new FileWriter(filepath);
         String json = gson.toJson(object);
